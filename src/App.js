7e7041fc51a95b239bf2eb4523eaac80329124f8
Thebornsinner <Ozuzumatobe@gmail.com>
2022-08-13 15:20:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './Components/Header';
+import Body from './Components/Body';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+ function Header() {
+  return(
+    <div>
+        <meta charset="utf-8"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+        <title>Key Electronics -Best Gadgets in The World</title>
+        <meta name="robots" content="noindex, follow" />
+        <meta name="description" content=""/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+       
+        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png"/>
+    
+        <link rel="preconnect" href="https://fonts.googleapis.com/"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&amp;family=Work+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&amp;display=swap" rel="stylesheet"/>
+    
+       
+        <link rel="stylesheet" href="css/vendor/bootstrap.min.css"/>
+    
+      
+        <link rel="stylesheet" href="css/plugins/swiper-bundle.min.css"/>
+        <link rel="stylesheet" href="css/plugins/font-awesome.min.css"/>
+        <link rel="stylesheet" href="css/plugins/simple-line-icons.css"/>
+        <link rel="stylesheet" href="css/plugins/fancybox.min.css"/>
+        <link rel="stylesheet" href="css/plugins/nice-select.css"/>
+    
+        <link rel="stylesheet" href="css/style.css"/>
+    </div>
+)
+}
+export default Header;
